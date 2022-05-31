21374fd2c043cd6b881a43afdade4dcf363903da
Samuel Prayoga Tampubolon <samueltampubolon2002@gmail.com>
2022-05-31 21:07:41 +0700

    URLConfig

--- app/src/main/java/com/project/delcanteen/util/Config.kt
+++ app/src/main/java/com/project/delcanteen/util/Config.kt
@@ -1,6 +1,6 @@
 package com.project.delcanteen.util
 
 object Config {
-    const val baseUrl2 = "http://192.168.43.162/tokoonline/public/"
+    const val baseUrl2 = "http://192.168.8.131/kantindel/public/"
     const val productUrl = baseUrl2 + "storage/produk/"
 }
